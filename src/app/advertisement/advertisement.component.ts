b4617fd9ad8547955ff3cf813146269e629f9dfb
mahdi221b <mahdi.dridi@esprit.tn>
2023-04-15 21:28:03 +0200

    CRUD ADS : getALL & DELETE

--- src/app/advertisement/advertisement.component.ts
+++ src/app/advertisement/advertisement.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdvertisementServiceService } from '../services/advertisement-service.service';
+import { Router } from '@angular/router';
+import { Advertisement } from '../model/Advertisement';
 
 @Component({
   selector: 'app-advertisement',
   templateUrl: './advertisement.component.html',
   styleUrls: ['./advertisement.component.css']
 })
-export class AdvertisementComponent {
+export class AdvertisementComponent implements OnInit{
 
+  listAds!:Advertisement[];
+
+  constructor(private advertisementService: AdvertisementServiceService,private route:Router) { }
+  ngOnInit(): void {
+    this.advertisementService.getAllAdvertisements().subscribe({next:(data) => 
+      this.listAds= data
+    });
+  }
+
+  delete(id: number){
+    this.advertisementService.deleteAdvertisement(id).subscribe({
+         next : () => this.listAds = this.listAds.filter((p)=>p.id != id)
+    })
+  }
 }
